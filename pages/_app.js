8dad83b59fd34aa35449185d68376eef34f07388
Amine Alameddine <60780857+chromodome@users.noreply.github.com>
2022-06-28 19:05:34 +0200

    update 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,20 @@
-import {  Bounce, ToastContainer } from 'react-toastify';
-import '@/styles/globals.css';
+import { Bounce, ToastContainer } from 'react-toastify';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
     return (
-
-            <div>
-                <ToastContainer
-                    className="mt-36"
-                    position="top-right"
-                    hideProgressBar={true}
-                    autoClose={1750}
-                    transition={Bounce}
-                    icon={false}
-                    theme="colored"
-                />
-                    <Component {...pageProps} />
-            </div>
+        <div>
+            <ToastContainer
+                className="mt-36"
+                position="top-right"
+                hideProgressBar={true}
+                autoClose={1750}
+                transition={Bounce}
+                icon={false}
+                theme="colored"
+            />
+            <Component {...pageProps} />
+        </div>
     );
 }
 
